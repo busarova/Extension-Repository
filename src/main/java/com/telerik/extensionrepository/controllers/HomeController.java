7c8044492434507e74d44a7d74978757a66c9a78
dyanakie <dyanakie@yahoo.com>
2018-08-16 18:56:09 +0300

    added features, new and popular in main page and controller (dummy)

--- src/main/java/com/telerik/extensionrepository/controllers/HomeController.java
+++ src/main/java/com/telerik/extensionrepository/controllers/HomeController.java
@@ -12,20 +12,39 @@ import java.util.List;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String showHomePage(){
 
         return "index";
-    }
+    }*/
 
-    @PostMapping
+    @GetMapping("/")
     public ModelAndView showFeatured(){
-        ModelAndView modelAndView = new ModelAndView();
+        ModelAndView modelAndView = new ModelAndView("index");
 
-        List<String> list = new ArrayList<>();
+        List<String> featured = new ArrayList<>();
 
+        featured.add("EXTENSIONS 1");
+        featured.add("EXTENSIONS 2");
+        featured.add("EXTENSIONS 3");
 
-        modelAndView.addObject("featured", list);
+        modelAndView.addObject("featured", featured);
+
+        List<String> popular = new ArrayList<>();
+
+        popular.add("POPULAR 1");
+        popular.add("POPULAR 2");
+        popular.add("POPULAR 3");
+
+        modelAndView.addObject("popular", popular);
+
+        List<String> mostNew = new ArrayList<>();
+
+        mostNew.add("MOST NEW 1");
+        mostNew.add("MOST NEW 2");
+        mostNew.add("MOST NEW 3");
+
+        modelAndView.addObject("mostNew", mostNew);
 
         return modelAndView;
     }
