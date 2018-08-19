42c45cf10acd906d726b00cec70932bb23ffbce7
dyanakie <dyanakie@yahoo.com>
2018-08-19 16:07:14 +0300

    updated database

--- exension_repository.sql
+++ exension_repository.sql
@@ -57,15 +57,15 @@ CREATE TABLE IF NOT EXISTS `extension` (
   `owner` varchar(50) NOT NULL,
   `numberOfDownloads` int(11) NOT NULL DEFAULT 0,
   `tags` varchar(50) NOT NULL,
-  `downloadFileID` int(11) NOT NULL,
-  `gitExtensionInfoID` int(11) NOT NULL,
-  `uploadDate` varchar(50) NOT NULL,
-  `featured` tinyint(2) NOT NULL,
+  `downloadFileID` int(11) DEFAULT NULL,
+  `gitExtensionInfoID` int(11) DEFAULT NULL,
+  `uploadDate` varchar(50) DEFAULT NULL,
+  `featured` tinyint(2) NOT NULL DEFAULT 1,
   `approved` tinyint(4) DEFAULT 1,
   PRIMARY KEY (`extensionID`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
--- Dumping data for table extension_repository.extension: ~10 rows (approximately)
+-- Dumping data for table extension_repository.extension: ~12 rows (approximately)
 /*!40000 ALTER TABLE `extension` DISABLE KEYS */;
 INSERT INTO `extension` (`extensionID`, `name`, `description`, `version`, `owner`, `numberOfDownloads`, `tags`, `downloadFileID`, `gitExtensionInfoID`, `uploadDate`, `featured`, `approved`) VALUES
 	(1, 'Apple Extension', 'bla bla bla blaaaaaaah', '1.0', 'drago', 3, 'super awesome', 1, 1, '16/08/2018', 0, 0),
@@ -76,22 +76,27 @@ INSERT INTO `extension` (`extensionID`, `name`, `description`, `version`, `owner
 	(6, 'Bravos Extension', 'JIVOTA E RADOST I TUGA ', '1.1', 'rosi', 4, 'asdasdasd', 6, 6, '09/08/2018', 0, 0),
 	(7, 'Test SuperTest', 'SUS 100 KILOMETRA V CHAS', '1.5', 'drago', 7, 'ksadklasda', 7, 7, '14/08/2018', 0, 0),
 	(8, 'Great Overall Extension', '100 patrona ', '1.3', 'pesho', 2, 'asdasdas', 8, 8, '15/08/2018', 1, 0),
-	(9, 'Roberto Extension', 'Karai si kolata lelelel', '1.1', 'pesho', 10, 'asdasdas', 9, 9, '16/08/2018', 1, 1),
-	(10, 'Britney Spears Extension', 'Oops I did it again', '1.2', 'pesho', 4, 'adasdasdasd', 10, 10, '15/08/2018', 0, 0);
+	(9, 'Roberto Extension', 'Karai si kolata lelelel', '1.1', 'pesho', 10, 'asdasdas', 9, 9, '16/08/2018', 1, 0),
+	(10, 'Britney Spears Extension', 'Oops I did it again', '1.2', 'pesho', 4, 'adasdasdasd', 10, 10, '15/08/2018', 0, 1),
+	(13, 'Gosho Extention', 'great overall ext', '1.0', 'gosho', 0, 'wow great theBest', NULL, 11, '15/08/2018', 0, 0),
+	(14, 'Sony Extention', 'New Tech Ext', '1.0', 'gosho', 0, 'sonyExt newExt', NULL, 12, '15/08/2018', 0, 0);
 /*!40000 ALTER TABLE `extension` ENABLE KEYS */;
 
 -- Dumping structure for table extension_repository.git_extension_info
 CREATE TABLE IF NOT EXISTS `git_extension_info` (
   `gitExtensionInfoID` int(11) NOT NULL AUTO_INCREMENT,
-  `openIssues` int(11) NOT NULL,
-  `pullRequests` int(11) NOT NULL,
-  `lastCommitDate` varchar(50) NOT NULL,
+  `openIssues` int(11) DEFAULT NULL,
+  `pullRequests` int(11) DEFAULT NULL,
+  `lastCommitDate` varchar(50) DEFAULT NULL,
   `gitRepoLink` varchar(100) NOT NULL,
   PRIMARY KEY (`gitExtensionInfoID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
--- Dumping data for table extension_repository.git_extension_info: ~0 rows (approximately)
+-- Dumping data for table extension_repository.git_extension_info: ~2 rows (approximately)
 /*!40000 ALTER TABLE `git_extension_info` DISABLE KEYS */;
+INSERT INTO `git_extension_info` (`gitExtensionInfoID`, `openIssues`, `pullRequests`, `lastCommitDate`, `gitRepoLink`) VALUES
+	(11, 0, 0, NULL, 'www.tctc.com'),
+	(12, 0, 0, NULL, 'www.sony.com');
 /*!40000 ALTER TABLE `git_extension_info` ENABLE KEYS */;
 
 -- Dumping structure for table extension_repository.users
@@ -106,8 +111,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
 	('drago', '{noop}123', 1),
-	('gosho', '{noop}pass2', 1),
-	('misho', '{noop}pass3', 1),
+	('gosho', '{noop}123', 1),
+	('misho', '{noop}123', 1),
 	('pesho', '{noop}123', 1),
 	('rosi', '{noop}123', 1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
