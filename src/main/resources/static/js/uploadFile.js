3c91c57f41add4c0c7f525b189015139407d70d6
R.Busarova <r.busarova@abv.bg>
2018-09-07 18:29:09 +0300

    Added styling to profil page.

---
+++ src/main/resources/static/js/uploadFile.js
@@ -0,0 +1,39 @@
+//Upload profile image
+$(document).ready(function() {
+
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.avatar').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+});
+
+// Create Extension Upload file
+function initializeFileUploads() {
+    $('.file-upload').change(function () {
+        var file = $(this).val();
+        $(this).closest('.input-group').find('.file-upload-text').val(file);
+    });
+    $('.file-upload-btn').click(function () {
+        $(this).find('.file-upload').trigger('click');
+    });
+    $('.file-upload').click(function (e) {
+        e.stopPropagation();
+    });
+}
+
+// On document load:
+$(function() {
+    initializeFileUploads();
+});
